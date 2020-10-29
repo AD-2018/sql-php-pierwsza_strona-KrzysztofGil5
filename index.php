2220d450b7a76ae6bc9cb7ed01a417cdffe2e87d
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-10-29 15:31:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,18 +7,16 @@ echo("Witaj świecie nazywam się Krzysztof Gil");
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-echo ("<h3>Zadanie 1</h3>");
+echo("<h1>Zadanie1</h1>");
 $sql = "SELECT * FROM pracownicy";
 
-$result = $mysqli_query($conn,$sql);
+$result = mysqli_query($conn,$sql);
 
-echo('<table border="1" class="tabela"');
-echo ("<tr><th>ID</th><th>Imię</th><th>Dział</th><th>Zarobki</th><th>Nazwa działu</tr>");
-while($row=mysqli_fetch_assoc($result)){
-    echo("<tr>");    
+echo("<table border=1>");
+echo("<tr><td>ID</td><td>Imię</td><td>Dział</td><td>Zarobki</td><td>Data Urodzenia</td></tr>");
+while($row=mysqli_fetch_assoc($result)) {
     echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td><td>".$row['imie']."</td><td>".$row['dzial']."</td><td>".$row['zarobki']."</td>"."<td>".$row['nazwa_dzial']."</td>");
-    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['dzial']."</td>"."<td>".$row['zarobki']."</td>"."<td>".$row['data_urodzenia']."</td>");
     echo("</tr>");
 }
 echo ("</table>");
