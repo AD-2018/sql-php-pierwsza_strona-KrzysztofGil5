416fbdb5651683ab017c4583d3d581fd4d7cae87
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-10-30 12:10:28 +0100

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+$servername ="sql7.freemysqlhosting.net";
+$username = "sql7373404";
+$password = "b1DIlA93DR";
+$dbname = "sql7373404";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
