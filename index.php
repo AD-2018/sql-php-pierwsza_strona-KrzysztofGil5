2cf0bc49b607bc62667f667fc31eb29bd6ad8d86
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-05 11:59:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
-require = "connect.php";
+$servername ="51.254.207.32";
+$username = "krzysztof";
+$password = "6qOc7ubC9iHsPRCH";
+$dbname = "krzysztof";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
