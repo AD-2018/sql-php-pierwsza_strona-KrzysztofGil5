cc85ebd47963fd4dcdc8bf80c7094e07e2ebb9e3
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-05 13:54:59 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-$servername ="51.254.207.32";
-$username = "krzysztof";
-$password = "6qOc7ubC9iHsPRCH";
-$dbname = "krzysztof";
+$servername ="mysql-krzysztofgil5.alwaysdata.net";
+$username = "217145";
+$password = "xtcaSmf3bE3gdRP4";
+$dbname = "krzysztofgil5_jd";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 ?>
