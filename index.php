99959d579a3a221ca617fe28a166f24fb2173b6f
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-05 14:06:23 +0100

    mezczyzni+kobiety

--- index.php
+++ index.php
@@ -8,23 +8,23 @@
 <?php
 require "connect.php";
 
+echo("<br>Witaj świecie nazywam się Krzysztof Gil<br>");
+
+echo("<br><h3>Wszyscy pracownicy</h3>");
+$sql = "SELECT * FROM pracownicy";
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
-$sql = "SELECT * FROM pracownicy";
 
 $result = mysqli_query($conn, $sql);
 if ( $result) {
-      echo "<li>ok";
+      echo "<li>Ok";
 } 
   else {
       echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
  
-echo("<br>Witaj świecie nazywam się Krzysztof Gil<br>");
-
-
-echo("<br><h3>Wszyscy pracownicy</h3>");
 echo("<table border=1>");
 echo("<tr><td>ID</td><td>Imię</td><td>Dział</td><td>Zarobki</td><td>Data Urodzenia</td></tr>");
 while($row=mysqli_fetch_assoc($result)) {
@@ -33,10 +33,23 @@ while($row=mysqli_fetch_assoc($result)) {
     echo("</tr>");
 }
 echo ("</table>");
- 
-$sql = "SELECT * FROM pracownicy WHERE imie NOT LIKE '%a'";
- 
+
 echo("<br><h3>Tylko mężczyźni</h3>");
+$sql = "SELECT * FROM pracownicy WHERE imie NOT LIKE '%a'";
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
+  
+  
+  $result = mysqli_query($conn, $sql);
+  if ( $result) {
+        echo "<li>Ok";
+  } 
+    else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
 echo("<table border=1>");
 echo("<tr><td>ID</td><td>Imię</td><td>Dział</td><td>Zarobki</td><td>Data Urodzenia</td></tr>");
 while($row=mysqli_fetch_assoc($result)) {
@@ -46,8 +59,29 @@ while($row=mysqli_fetch_assoc($result)) {
 }
 echo ("</table>");
 
+echo("<br><h3>Tylko kobiety</h3>");
+$sql = "SELECT * FROM pracownicy WHERE imie LIKE '%a'";
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  }
   
   
-?>
+  $result = mysqli_query($conn, $sql);
+  if ( $result) {
+        echo "<li>Ok";
+  } 
+    else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+  }
+
+echo("<table border=1>");
+echo("<tr><td>ID</td><td>Imię</td><td>Dział</td><td>Zarobki</td><td>Data Urodzenia</td></tr>");
+while($row=mysqli_fetch_assoc($result)) {
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['dzial']."</td>"."<td>".$row['zarobki']."</td>"."<td>".$row['data_urodzenia']."</td>");
+    echo("</tr>");
+}
+echo ("</table>");
 </body>
 </html>
