234a9115126fbb8d6b445c5016ecf7b0664e240e
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-05 15:04:31 +0100

    Update funcAgregujace.php

--- funcAgregujace.php
+++ funcAgregujace.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<div class="nav">
+   <a href="orgPacownicy.php">orgPracownicy</a>
+   <a href="funcAgregujace.php">funcAgregujace</a>
+</div>
 <?php
 echo("tu jestem: funcAgregujace.php");
 ?>
+</body>
+</html>
