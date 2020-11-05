b4a2470e3dba36a05136fb52ec3ec4c82cee7eb5
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-05 13:58:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if ( $result) {
 echo("<br>Witaj świecie nazywam się Krzysztof Gil<br>");
 
 
-  
+echo("<br><h3>Wszyscy pracownicy</h3>");
 echo("<table border=1>");
 echo("<tr><td>ID</td><td>Imię</td><td>Dział</td><td>Zarobki</td><td>Data Urodzenia</td></tr>");
 while($row=mysqli_fetch_assoc($result)) {
@@ -33,6 +33,21 @@ while($row=mysqli_fetch_assoc($result)) {
     echo("</tr>");
 }
 echo ("</table>");
+ 
+$sql = "SELECT * FROM pracownicy WHERE imie NOT LIKE '%a'";
+ 
+echo("<br><h3>Tylko mężczyźni</h3>");
+echo("<table border=1>");
+echo("<tr><td>ID</td><td>Imię</td><td>Dział</td><td>Zarobki</td><td>Data Urodzenia</td></tr>");
+while($row=mysqli_fetch_assoc($result)) {
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['dzial']."</td>"."<td>".$row['zarobki']."</td>"."<td>".$row['data_urodzenia']."</td>");
+    echo("</tr>");
+}
+echo ("</table>");
+
+  
+  
 ?>
 </body>
 </html>
