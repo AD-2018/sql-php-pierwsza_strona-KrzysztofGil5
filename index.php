896cf278d308483c4d064ea51e9681a18f892d1d
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-05 14:22:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -27,38 +27,5 @@ while($row=mysqli_fetch_assoc($result)) {
     echo("</tr>");
 }
 echo ("</table>");
-
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-echo("<br><h3>Tylko mężczyźni</h3>");
-
-$sql = "SELECT * FROM pracownicy WHERE imie NOT LIKE '%a'";
-
-$result = mysqli_query($conn, $sql);
-
-echo("<table border=1>");
-echo("<tr><td>ID</td><td>Imię</td><td>Dział</td><td>Zarobki</td><td>Data Urodzenia</td></tr>");
-while($row=mysqli_fetch_assoc($result)) {
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['dzial']."</td>"."<td>".$row['zarobki']."</td>"."<td>".$row['data_urodzenia']."</td>");
-    echo("</tr>");
-}
-echo ("</table>");
-
-// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-echo("<br><h3>Tylko kobiety</h3>");
-$sql = "SELECT * FROM pracownicy WHERE imie LIKE '%a'";
-
-$result = mysqli_query($conn, $sql);
-
-echo("<table border=1>");
-echo("<tr><td>ID</td><td>Imię</td><td>Dział</td><td>Zarobki</td><td>Data Urodzenia</td></tr>");
-while($row=mysqli_fetch_assoc($result)) {
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['dzial']."</td>"."<td>".$row['zarobki']."</td>"."<td>".$row['data_urodzenia']."</td>");
-    echo("</tr>");
-}
-echo ("</table>");
 </body>
 </html>
