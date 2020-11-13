fb144874d64095e2a240bc429e28e25c097e46ee
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-13 13:16:30 +0100

    Update formatowaniedat.php

--- formatowaniedat.php
+++ formatowaniedat.php
@@ -87,7 +87,76 @@ echo("<table border=1>");
 echo("<tr><th>Aktualna godzina</th></tr>");
 while($row=mysqli_fetch_assoc($result)) {
     echo("<tr>");
-    echo("<td>".$row['czas']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['zarobki']."</td>"."<td>".$row['miesiac']."</td>");
+    echo("<td>".$row['czas']."</td>");
+    echo("</tr>");
+}
+echo ("</table>");
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+echo("<br><h3>Wyświetl datę urodzenia w formie: ROK-MIESIĄC-DZIEŃ</h3>");
+
+$sql = "SELECT *, DATE_FORMAT(data_urodzenia,'%Y-%M-%W') as 'data' from pracownicy";
+echo(".$sql");
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+    echo "<li>Ok";
+}
+else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+echo("<table border=1>");
+echo("<tr><th>ID</th><th>Imię</th><th>Zarobki</th><th>Data Urodzenia</th></tr>");
+while($row=mysqli_fetch_assoc($result)) {
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['zarobki']."</td>"."<td>".$row['data']."</td>");
+    echo("</tr>");
+}
+echo ("</table>");
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+echo("<br><h3>Ile godzin, minut już żyjesz</h3>");
+
+$sql = "SELECT imie,DATEDIFF(CURDATE(),data_urodzenia) as dni, DATEDIFF(CURDATE(),data_urodzenia)*24 as godziny, DATEDIFF(CURDATE(),data_urodzenia)*24*60 as minuty FROM pracownicy";
+echo(".$sql");
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+    echo "<li>Ok";
+}
+else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+echo("<table border=1>");
+echo("<tr><th>ID</th><th>Imię</th><th>Dni</th><th>Godziny</th><th>Minuty</th></tr>");
+while($row=mysqli_fetch_assoc($result)) {
+    echo("<tr>");
+    echo("<td>".$row['id_pracownicy']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['dni']."</td>"."<td>".$row['godziny']."</td>"."<td>".$row['minuty']."</td>");
+    echo("</tr>");
+}
+echo ("</table>");
+
+// -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+echo("<br><h3>W którym dniu roku urodziłeś się</h3>");
+
+$sql = "SELECT DATE_FORMAT("2002-03-09", "%j") as urodzenie";
+echo(".$sql");
+$result = mysqli_query($conn, $sql);
+if ( $result) {
+    echo "<li>Ok";
+}
+else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+echo("<table border=1>");
+echo("<tr><th>Dzien narodzin</th></tr>");
+while($row=mysqli_fetch_assoc($result)) {
+    echo("<tr>");
+    echo("<td>".$row['urodzenie']."</td>");
     echo("</tr>");
 }
 echo ("</table>");
