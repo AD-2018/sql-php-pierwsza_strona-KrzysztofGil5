1677e7b83e99f9b9a3c4f8996bd923badeb6accd
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-19 14:23:15 +0100

    Create insert.php

---
+++ insert.php
@@ -0,0 +1,17 @@
+<?php
+echo("jestes w insert.php");
+echo $_POST['name'];
+
+require "connect.php";
+
+$sql = "INSERT INTO Pracownik (null, $_POST['name'], dzial, zarobki) 
+       VALUES (null,'Ksawery', 3, 36,'1995-10-21')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
