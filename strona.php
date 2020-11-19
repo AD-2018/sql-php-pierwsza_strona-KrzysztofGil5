11c34885a9507bfba4026008ad1f5a91c8db9297
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-19 14:16:30 +0100

    Update strona.php

--- strona.php
+++ strona.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Krzysztof - Klauzula Having</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>Krzysztof Gil nr 5</h1>
+    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-KrzysztofGil5">Github</a><br>
+        <div class="nav">
+        <a href="pracownicy.php">Pracownicy</a>
+        <a href="index.php">Pracownicy i Organizacja</a>
+        <a href="funcAgregujace.php">Funkcje Agregujące</a>
+        <a href="sortowanie.php">Sortowanie</a>
+        <a href="groupby.php">Group By</a>
+        <a href="limit.php">Limit</a>
+        <a href="having.php">Klauzula Having</a>
+        <a href="dataiczas.php">Data i czas</a>
+        <a href="formatowaniedat.php">Formatowanie dat</a>
+        <a href="formularz.html">Formularz</a>
+    </div>
 <?php
 	echo("jesteś na stronie.php");
 
@@ -7,3 +28,5 @@
 
 	echo("<ul>");
 ?>
+</body>
+</html>
