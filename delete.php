373714281247cc3214190358321e8f37ee4b003f
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-26 14:48:35 +0100

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,27 @@
+<?php
+echo("jestes w delete.php <br>");
+echo("<br>");
+echo $_POST['id_pracownicy'];
+
+
+require "connect.php";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+//definiujemy zapytanie $sql
+$sql = "DELETE FROM pracownicy WHERE id_pracownicy=".$_POST['id_pracownicy'];
+
+//wyświetlamy zapytanie $sql
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "<br>Record deleted successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
