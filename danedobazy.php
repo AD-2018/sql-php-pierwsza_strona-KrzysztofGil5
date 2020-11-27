0a6a034532ba0fcdf21047ffd1a7b81d8a768711
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-11-27 12:01:13 +0100

    Update danedobazy.php

--- danedobazy.php
+++ danedobazy.php
@@ -33,25 +33,22 @@ require "connect.php";
 
 echo("<br><h3>Wszyscy Pracownicy</h3>");
 $sql = "SELECT * FROM pracownicy";
+echo ("<li>".$sql);
+  $result = mysqli_query($conn, $sql);
+    if ( $result) {
+        echo "<li>ok";
+    } else {
+      echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+  
 
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+echo('<table border="1">');
+echo('<th>Id</th><th>Imie</th><th>zarobki</th><th>dzial</th><th>Data urodzenia</th>');
 
-$result = mysqli_query($conn, $sql);
-if ( $result) {
-    echo "<li>Ok";
-}
-else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
-
-echo("<table border=1>");
-echo("<tr><th>ID</th><th>Imię</th><th>Zarobki</th><th>Data Urodzenia</th></tr>");
-while($row=mysqli_fetch_assoc($result)) {
-    echo("<tr>");
-    echo("<td>".$row['id_pracownicy']."</td>"."<td>".$row['imie']."</td>"."<td>".$row['zarobki']."</td>"."<td>".$row['data_urodzenia']."</td>"
-	 	     
+    while($row=mysqli_fetch_assoc($result)){
+        echo('<tr>');
+        echo('<td>'.$row['id_pracownicy'].'</td>'.'<td>'.$row['imie'].'</td>'.'<td>'.$row['zarobki'].'</td>'.'<td>'.$row['dzial'].'</td>'.'<td>'.$row['data_urodzenia'].'</td>'.
+	     
 	     '<td>
 	    
 	     <form action="delete.php" method="POST">
@@ -60,10 +57,10 @@ while($row=mysqli_fetch_assoc($result)) {
 	</form>
 	     
 	     </td>');
-	   
-    echo("</tr>");
-}
-echo ("</table>");
+	    
+        echo('</tr>');
+    }
+  echo('</table>');
 ?>
 </body>
 </html>
