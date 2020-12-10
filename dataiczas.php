b4e40c1e6fb18372aabbb78daa21aac9c812a295
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-12-10 11:09:29 +0100

    Update dataiczas.php

--- dataiczas.php
+++ dataiczas.php
@@ -8,18 +8,7 @@
     <h1>Krzysztof Gil nr 5</h1>
     <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-KrzysztofGil5">Github</a><br>
         <div class="nav">
-        <a href="pracownicy.php">Pracownicy</a>
-        <a href="index.php">Pracownicy i Organizacja</a>
-        <a href="funcAgregujace.php">Funkcje Agregujące</a>
-        <a href="sortowanie.php">Sortowanie</a>
-        <a href="groupby.php">Group By</a>
-        <a href="limit.php">Limit</a>
-        <a href="having.php">Klauzula Having</a>
-        <a href="dataiczas.php">Data i czas</a>
-        <a href="formatowaniedat.php">Formatowanie dat</a>
-        <a href="formularz.html">Formularz</a>
-        <a href="insert.php">Insert</a>
-        <a href="danedobazy.php">Dane do bazy</a>
+        <a href="index.html">Powrót</a>
     </div>
 <?php
 require "connect.php";
