51a9dc04e69c037a685bc252a7e38c607f37a665
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2020-12-17 10:10:15 +0100

    Update pracownicy.php

--- pracownicy.php
+++ pracownicy.php
@@ -5,11 +5,13 @@
 <link rel="stylesheet" href="style.css">
 </head>
 <body>
+    <div class="banner">
     <h1>Krzysztof Gil nr 5</h1>
-    <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-KrzysztofGil5">Github</a><br>
+  </div>
         <div class="nav">
         <a href="index.php">Powrót</a>
     </div>
+<div class="tabele">
 <?php
 require "connect.php";
 echo("Jestem w: Pracownicy");
@@ -83,5 +85,6 @@ while($row=mysqli_fetch_assoc($result)) {
 }
 echo ("</table>");
 ?>
+  </div>
 </body>
 </html>
