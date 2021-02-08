c6fd52ea92989cecdd7dbfd821f6e1baa1c380b9
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2021-02-08 10:17:06 +0100

    Create deltytul.php

---
+++ biblioteka/deltytul.php
@@ -0,0 +1,18 @@
+<?php
+echo $_POST['id'];
+
+require_once("https://krzysztof-php.herokuapp.com/pracownicy/connect.php");
+
+$sql = "DELETE FROM bibl_tytul WHERE id_tytul=".$_POST['id'];
+
+echo $sql;
+
+if ($conn->query($sql) === TRUE) {
+  echo "Record deleted successfully";
+  header('Location: https://stanikjoanna.herokuapp.com/ksiazki/ksiazki.php');
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
