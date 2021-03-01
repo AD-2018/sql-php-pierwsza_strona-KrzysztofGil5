ff2938e959696fcb97f0360b55910fb845e85d9e
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2021-03-01 01:15:24 +0100

    Update funcAgregujace.php

--- pracownicy/funcAgregujace.php
+++ pracownicy/funcAgregujace.php
@@ -13,7 +13,7 @@
     </div>
   <div class="tabele">
 <?php
-require "connect.php";
+require "../connect.php";
 echo("Jestem w: Funkcje Agregujące");
 
 echo("<br><h3>Suma zarobków wszystkich pracowników</h3>");
