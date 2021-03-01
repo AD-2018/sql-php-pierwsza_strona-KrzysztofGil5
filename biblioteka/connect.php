5c82cef3089b248b60d6d30d21223138d0edb141
Krzysztof Gil <73647789+KrzysztofGil5@users.noreply.github.com>
2021-03-01 01:12:44 +0100

    Delete connect.php

--- biblioteka/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername ="mysql-krzysztofgil5.alwaysdata.net";
-$username = "217145";
-$password = "xtcaSmf3bE3gdRP4";
-$dbname = "krzysztofgil5_jd";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-?>
