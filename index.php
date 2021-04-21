7b6ef80923135d2f91b4759a821849b3333bb092
KrzysztofGil5 <krzysztof.gil10@gmail.com>
2021-04-21 22:32:49 +0200

    .

--- index.php
+++ index.php
@@ -9,19 +9,7 @@
     <h1>Krzysztof Gil nr 5</h1>
     </div>
     <div class="nav">
-        <a href="https://github.com/AD-2018/sql-php-pierwsza_strona-KrzysztofGil5">Github</a>
-        <a href="pracownicy/pracownicy.php">Pracownicy</a>
-        <a href="pracownicy/funcAgregujace.php">Funkcje Agregujące</a>
-        <a href="pracownicy/sortowanie.php">Sortowanie</a>
-        <a href="pracownicy/groupby.php">Group By</a>
-        <a href="pracownicy/limit.php">Limit</a>
-        <a href="pracownicy/having.php">Klauzula Having</a>
-        <a href="pracownicy/dataiczas.php">Data i czas</a>
-        <a href="pracownicy/formatowaniedat.php">Formatowanie dat</a>
-        <a href="pracownicy/danedobazy.php">Dane do bazy</a>
-        <a href="biblioteka/biblioteka.php">Biblioteka</a>
-        <a href="flexbox/index.html">Flexbox</a>
-        <a href="cssgrid/index.html">CSS Grid</a>
+    <?php require_once("menu.php"); ?>
     </div>
 <div class="tabele">
 <?php
---
+++ menu.php
@@ -0,0 +1,15 @@
+<ul>
+        <li><a href="https://github.com/AD-2018/sql-php-pierwsza_strona-KrzysztofGil5">Github</a></li>
+        <li><a href="pracownicy/pracownicy.php">Pracownicy</a></li>
+        <li><a href="pracownicy/funcAgregujace.php">Funkcje Agregujące</a></li>
+        <li><a href="pracownicy/sortowanie.php">Sortowanie</a></li>
+        <li><a href="pracownicy/groupby.php">Group By</a></li>
+        <li><a href="pracownicy/limit.php">Limit</a></li>
+        <li><a href="pracownicy/having.php">Klauzula Having</a></li>
+        <li><a href="pracownicy/dataiczas.php">Data i czas</a></li>
+        <li><a href="pracownicy/formatowaniedat.php">Formatowanie dat</a></li>
+        <li><a href="pracownicy/danedobazy.php">Dane do bazy</a></li>
+        <li><a href="biblioteka/biblioteka.php">Biblioteka</a></li>
+        <li><a href="flexbox/index.html">Flexbox</a></li>
+        <li><a href="cssgrid/index.html">CSS Grid</a></li>
+    </ul>
